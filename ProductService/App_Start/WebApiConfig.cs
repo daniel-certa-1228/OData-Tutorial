89062aaebdf6baf8e470c84da2dc6608bb0113e2
Dan Certa <dcerta@oneeightycapital.com>
2018-08-21 09:50:34 -0500

    Up To "Querying the Entity Set

--- ProductService/App_Start/WebApiConfig.cs
+++ ProductService/App_Start/WebApiConfig.cs
@@ -2,6 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
+using ProductService.Models;
+using Microsoft.AspNet.OData.Builder;
+using Microsoft.AspNet.OData.Extensions;
 
 namespace ProductService
 {
@@ -9,16 +12,12 @@ namespace ProductService
     {
         public static void Register(HttpConfiguration config)
         {
-            // Web API configuration and services
-
-            // Web API routes
-            config.MapHttpAttributeRoutes();
-
-            config.Routes.MapHttpRoute(
-                name: "DefaultApi",
-                routeTemplate: "api/{controller}/{id}",
-                defaults: new { id = RouteParameter.Optional }
-            );
+            ODataModelBuilder builder = new ODataConventionModelBuilder();
+            builder.EntitySet<Product>("Products");
+            config.MapODataServiceRoute(
+                routeName: "ODataRoute",
+                routePrefix: null,
+                model: builder.GetEdmModel());
         }
     }
 }
---
+++ ProductService/Controllers/ProductsController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNet.OData;
+using ProductService.Models;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace ProductService.Controllers
+{
+    public class ProductsController : ODataController
+    {
+        ProductsContext db = new ProductsContext();
+        private bool ProductExists(int key)
+        {
+            return db.Products.Any(p => p.Id == key);
+        }
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
